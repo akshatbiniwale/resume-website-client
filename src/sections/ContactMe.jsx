178376001335613b83c55dd17290e93d7934e758
Akshat Biniwale <akshatbiniwale@gmail.com>
2025-04-14 00:41:58 +0530

    backend link change

--- src/sections/ContactMe.jsx
+++ src/sections/ContactMe.jsx
@@ -22,13 +22,16 @@ const ContactMe = () => {
 		setStatus("Sending...");
 
 		try {
-			const response = await fetch("http://localhost:5000/contact", {
-				method: "POST",
-				headers: {
-					"Content-Type": "application/json",
-				},
-				body: JSON.stringify(formData),
-			});
+			const response = await fetch(
+				`${import.meta.env.VITE_BACKEND_URL}/contact`,
+				{
+					method: "POST",
+					headers: {
+						"Content-Type": "application/json",
+					},
+					body: JSON.stringify(formData),
+				}
+			);
 
 			const data = await response.json();
 			if (data.success) {
